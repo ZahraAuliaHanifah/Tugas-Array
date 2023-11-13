29b4c97755c24d3467801bc8edce15e13906c668
ZahraAuliaHanifah <146632809+ZahraAuliaHanifah@users.noreply.github.com>
2023-11-13 14:59:53 +0700

    Add files via upload

---
+++ looping.java
@@ -0,0 +1,14 @@
+public class looping {
+    public static void main(String[] args) {
+        int y=0;
+        String S=0;
+        String O=0;
+        for (int S; i=0; i<=3; i++) {
+            for (int O; z=0; z>=1; z++) {
+                y = S+O;
+                System.out.print(y + " ");
+            }
+        }
+        System.out.println();
+    }
+}
